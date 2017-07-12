4808a9c5b3d8184bd414a4a3ee24e70dd9918ef9
Rémi Dupré <remim.dupre@gmail.com>
2017-07-13 00:04:21 +0200

    Rechargement lorsqu'un article est modifié

--- blog/articles/__init__.py
+++ blog/articles/__init__.py
@@ -6,18 +6,23 @@ import os
 import yaml
 import markdown2
 
+import django.utils.autoreload
+
 
 # Loads informations about articles from index.yaml
 root_dir = os.path.dirname(__file__)
 desc_file = root_dir + '/index.yaml'
 
+django.utils.autoreload._cached_filenames.append(desc_file)
 with open(desc_file, 'r') as f:
     descs = yaml.load(f)
 
 # Read articles content
 for art_key in descs :
     try:
-        with open(root_dir + '/' + descs[art_key]['content'], 'r') as f :
+        filename = root_dir + '/' + descs[art_key]['content']
+        django.utils.autoreload._cached_filenames.append(filename)
+        with open(filename, 'r') as f :
             descs[art_key]['content'] = f.read()
             descs[art_key]['html'] = markdown2.markdown(
                 descs[art_key]['content'],
